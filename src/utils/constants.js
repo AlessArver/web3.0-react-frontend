f60ea55808cbd2c36563813e31abfb2eed6fc208
alessarver <alessarver@gmail.com>
2022-05-10 14:14:13 +0300

    deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactinos.json";
 
 export const contractABI = abi.abi;
-export const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
+export const contractAddress = "0xBE3F217fEB3191d73e26869d02E9B7eFdD0E93fe";
